7ea4ad0aed141d0aa1e33652f1665a16d6721300
Kenny Dunn <kennydunn@live.co.uk>
2023-10-18 18:10:43 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,29 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "prod-vpc" {
  cidr_block = "10.0.0.0/16"
+ enable_dns_support = “true” #gives you an internal domain name
+ enable_dns_hostnames = “true” #gives you an internal host name
  
  tags = {
    Name = "Project VPC"
  }
+}
+
+resource “aws_subnet” “prod-subnet-public-1” {
+    vpc_id = “${aws_vpc.prod-vpc.id}”
+    cidr_block = “10.0.1.0/24”
+    map_public_ip_on_launch = “true” //it makes this a public subnet
+    availability_zone = “eu-west-1a”
+    tags {
+        Name = “prod-subnet-public-1”
+    }
+}
+
+resource “aws_subnet” “prod-subnet-public-2” {
+    vpc_id = “${aws_vpc.prod-vpc.id}”
+    cidr_block = “10.0.2.0/24”
+    map_public_ip_on_launch = “true” //it makes this a public subnet
+    availability_zone = “eu-west-1b”
+    tags {
+        Name = “prod-subnet-public-2”
+    }
 }
