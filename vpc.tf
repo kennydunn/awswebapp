62fa466ad0c90af2aceeb8a65b1152649d8222b3
Kenny Dunn <kennydunn@live.co.uk>
2023-10-18 18:18:57 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "prod-subnet-public-1" {
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
     availability_zone = "eu-west-1a"
-    tags {
+    tags = {
         Name = "prod-subnet-public-1"
     }
 }
@@ -23,7 +23,7 @@ resource "aws_subnet" "prod-subnet-public-2" {
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
     availability_zone = "eu-west-1b"
-    tags {
+    tags = {
         Name = "prod-subnet-public-2"
     }
 }
